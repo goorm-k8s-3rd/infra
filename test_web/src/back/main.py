95c5634ea96ca50ad7d26090b090bee855ce8e9c
hsh108868 <pusrty@gmail.com>
2023-03-24 18:33:41 +0900

    update test web

---
+++ test_web/src/back/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector
+import os
+
+app = FastAPI()
+
+origins = [
+    "http://localhost"
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+db_address = os.environ['DB_ADDRESS']
+db_port = os.environ['DB_PORT']
+db_name = os.environ['DB_NAME']
+db_password = os.environ['DB_PASSWORD']
+db_dbname = os.environ['DB_DBNAME']
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users")
+def read_users():
+    mydb = mysql.connector.connect(
+        host=db_address,
+        port=db_port,
+        user=db_name,
+        password=db_password,
+        database=db_dbname
+    )
+
+    cursor = mydb.cursor()
+    cursor.excute("SELECT * FROM users")
+    result = cursor.fetchall()
+    return result
---
+++ test_web/src/my-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
